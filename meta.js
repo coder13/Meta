b6284c5055a8c3388c9a1c2abf8baaf19655c033
Caleb Hoover <caleb@Bently-Jensens-MacBook.local>
2014-08-27 10:15:17 -0700

    initial

---
+++ meta.js
@@ -0,0 +1,22 @@
+var c = 'colors';
+
+var file = process.argv[2],
+	colors = require(c),
+	fs = require('fs'),
+	esprima = require('esprima'),
+	estraverse_asd = require('estraverse');
+
+console.log(('processing ' + file + '\n').blue);
+input = fs.readFileSync(file);
+ast = esprima.parse(input);
+
+console.log('this file requires: '.green);
+estraverse_asd.traverse(ast, {
+
+	enter: function (node) {
+		if (node.type == 'CallExpression' && node.callee.name == 'require') {
+			console.log('-', String(node.arguments[0].value).blue);
+		}
+	}
+});
+
