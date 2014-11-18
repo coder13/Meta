a8d8beafb88fe1c25394690eb5ea7b28d2f2275b
Caleb Hoover <caleb@Bently-Jensens-MacBook.local>
2014-11-18 15:59:21 -0800

    changes

--- meta.js
+++ meta.js
@@ -7,27 +7,17 @@ var mainFile = process.argv[2],
 	resolve = require('resolve');
 	curdir = String(process.cwd() + '/' + process.argv[2]).split('/').slice(0, -1).join('/');
 
-<<<<<<< HEAD
 console.log(curdir.white);
-=======
 hapi = require('hapi');
->>>>>>> 8647aa17eab342e82bc1d0b5a638d806964fac8b
 
 console.log(curdir.green);
 
-<<<<<<< HEAD
 var lookuptable = {},
 	requires = {};
 
 function getRequires(file, scope) {
-	curdir = String(file).split('/').slice(0, -1).join('/');
-=======
-var requires = {};
-
-function getRequires(file, scope) {
 	curdir = file.split('/').slice(0, -1).join('/');
 	console.log(curdir.green);
->>>>>>> 8647aa17eab342e82bc1d0b5a638d806964fac8b
 
 	scope = scope||' -- ';
 
@@ -49,9 +39,7 @@ function getRequires(file, scope) {
 					req[p] = getRequires(fullPath, scope + ' -- ');
 				}
 			} catch (e) {
-				
 				console.error(e);
-				
 			}
 		}
 	}
@@ -82,11 +70,7 @@ function getRequires(file, scope) {
 					pkg = vars[node.arguments[0].name];
 				}
 
-<<<<<<< HEAD
-				if (pkg) { // pkg can sometimes be undefind. 
-
-					_resolve(pkg);
-=======
+				_resolve(pkg);
 				console.log(String(pkg));
 				// Do we actually have a package we are looking at
 				if (pkg) {
@@ -107,34 +91,17 @@ function getRequires(file, scope) {
 							console.log(String(pkg).red);
 						}
 					}
->>>>>>> 8647aa17eab342e82bc1d0b5a638d806964fac8b
 				}
 			}
 		}
 	});
 
 	return req;
-
 }
 
-<<<<<<< HEAD
 function pop(path, l) {
 	return path.split('/').slice(0,-(l || 1)).join('/');
 }
 
 requires = getRequires(mainFile);
 console.log(requires);
-=======
-function resolve(pkg) {
-	var s = pkg.split('.');
-	if (pkg[0] == '.') {
-		if (s[s.length-1] == 'js' ? true : (fs.exists(pkg) || fs.exists(pkg + ".js"))) {
-		}
-			 console.log(pkg.orange);
-	}
-	return require.resolve(pkg, {basedir: curdir});
-}
-
-requires = getRequires(file);
-console.log(JSON.stringify(requires));
->>>>>>> 8647aa17eab342e82bc1d0b5a638d806964fac8b
